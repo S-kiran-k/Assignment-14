c22cef12d950185a238a79858611a3bf4bee22fa
S-kiran-k <kiransrinivasan03@gmail.com>
2024-03-30 17:31:31 +0530

    intial commit

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-// function multiplyByTen(myArray) {
-//     // Use the map() method to create a new array with each element multiplied by 10
-//     const resultArray = myArray.map((data)=>{
-//         return data;
-        
-//     })
-//     return resultArray
-// }
 
-// // Sample Input 1
-// const input1 = [12, 2, 2, 4, 1];
-// const output1 = multiplyByTen(input1);
-// console.log("Sample Output 1:", output1);
+// map
+function multiplyByTen(myArray) {
+    // Use the map() method to create a new array with each element multiplied by 10
+    const resultArray = myArray.map((data)=>{
+        return data;
+        
+    })
+    return resultArray
+}
+
+
+//Filter
+
+// Sample Input 1
+const input1 = [12, 2, 2, 4, 1];
+const output1 = multiplyByTen(input1);
+console.log("Sample Output 1:", output1);
 
 // Function to filter out even numbers from an array
 function filterEvenNumbers(myArray) {
@@ -22,6 +27,6 @@ function filterEvenNumbers(myArray) {
 }
 
 // Sample Input 1
-const input1 = [12, 5, 7, 8, 3, 2];
-const output1 = filterEvenNumbers(input1);
+const input2 = [12, 5, 7, 8, 3, 2];
+const output2 = filterEvenNumbers(input1);
 console.log("Sample Output 1:", output1);
